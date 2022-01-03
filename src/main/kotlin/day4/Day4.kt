65e52eddf6f5f5a9908bfa750f097431d59b7879
Gustaf Johansson <Gustaf.Johansson@cpacsystems.se>
2022-01-03 19:18:02 +0100

    add day4

---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,93 @@
+package day4
+
+import day4.Board.Companion.BOARD_WIDTH
+import util.Util
+import java.io.File
+
+fun main() {
+    val bingoData = File("src/main/kotlin/day4/input").readLines()
+    val bingo = parseBingo(bingoData)
+    Util.log(
+        "play bingo with squid",
+        System.currentTimeMillis(),
+        play(bingo)
+    )
+}
+
+fun parseBingo(bingoData: List<String>): Bingo {
+    //first line contains the random numbers from which we draw
+    val numbers = bingoData[0].split(",").map { it.toInt() }
+
+    val boards = bingoData.asSequence()
+        // remove first two lines from the input
+        .drop(2)
+        // split each number into elements and remove elements who are empty (happens on single digits)
+        .map { it.split(" ").filter { nr -> nr != "" } }
+        // filter out the empty lines between boards
+        .filter { it.isNotEmpty() }
+        // convert all numbers from String to Int
+        .map { it.map { nr -> nr.toInt() } }
+        // take five rows at a time
+        .chunked(BOARD_WIDTH)
+        // flatten the five rows into a single list of values, and map to Board
+        .map { Board(it.flatten()) }
+        .toList()
+    return Bingo(numbers, boards)
+}
+
+fun play(bingo: Bingo): Int {
+    bingo.numbers.forEach { number ->
+        bingo.boards.forEach { board ->
+            val index = board.findIndexOfValue(number)
+            if (index != -1) {
+                board.mark(index)
+
+                if (board.isWinner()) {
+                    return board.calculateScore(number)
+                }
+            }
+        }
+    }
+    error("No winner :(")
+}
+
+data class Bingo(val numbers: List<Int>, val boards: List<Board>)
+
+data class Board(val values: List<Int>) {
+    companion object {
+        const val BOARD_WIDTH = 5
+    }
+
+    private val marks = MutableList(BOARD_WIDTH * BOARD_WIDTH) { 0 }
+
+    fun getX(index: Int): Int = index % BOARD_WIDTH
+    fun getY(index: Int): Int = index / BOARD_WIDTH
+
+    fun findIndexOfValue(number: Int): Int {
+        return values.indexOfFirst { it == number }
+    }
+
+    fun getMarksRow(row: Int): List<Int> = marks.slice(row * BOARD_WIDTH until row * BOARD_WIDTH + BOARD_WIDTH)
+    fun getMarksCol(col: Int): List<Int> = marks.slice((0 + col) until marks.size step BOARD_WIDTH)
+
+    fun mark(index: Int) {
+        marks[index] = 1
+    }
+
+    fun isWinner(): Boolean {
+        val row = (0 until BOARD_WIDTH)                     // for each row of the board
+            .map { getMarksRow(it).sum() == BOARD_WIDTH }   // sum of a win row is 5 (1+1+1+1+1)
+            .find { it } ?: false                           // if any row is 5 -> win!
+
+        val col = (0 until BOARD_WIDTH)                     // for each column of the board
+            .map { getMarksCol(it).sum() == BOARD_WIDTH }   // sum of a win col is 5 (1+1+1+1+1)
+            .find { it } ?: false                           // if any col is 5 -> win!
+        return row or col
+    }
+
+    fun calculateScore(number: Int): Int {
+        return number * values.asSequence()
+            .filterIndexed { index, _ -> marks[index] == 0 }
+            .sumBy { it }
+    }
+}

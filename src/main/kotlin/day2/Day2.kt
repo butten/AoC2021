7c371efeceda9e0e7a2c5d12aa5009b964fde075
Gustaf Johansson <Gustaf.Johansson@cpacsystems.se>
2022-01-04 15:35:59 +0100

    add tests for day2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -3,63 +3,65 @@ package day2
 import util.Util
 import java.io.File
 
-fun main() {
-    val file = File("src/main/kotlin/day2/input")
-    val commands = Util.log(
-        "parse commands",
-        System.currentTimeMillis(),
-        parseCommands(file)
-    ) as Commands
-    Util.log(
-        "execute simple planned course",
-        System.currentTimeMillis(),
-        executeSimplePlannedCourse(commands).multiply()
-    )
-    Util.log(
-        "execute simple planned course",
-        System.currentTimeMillis(),
-        executeAdvancedPlannedCourse(commands).multiply()
-    )
-}
-
-data class Commands(val instructions: List<String>, val args: List<Int>)
-
-fun parseCommands(file: File): Commands {
-    val commands = file.readLines()
-    val instructions = commands.map { it.substringBefore(" ") }
-    val args = commands.map { it.substringAfter(" ").toInt() }
-    return Commands(instructions, args)
-}
-
-fun executeSimplePlannedCourse(commands: Commands): Position {
-    val position = Position(0, 0, 0)
-
-    (commands.instructions zip commands.args).forEach {
-        when (it.first) {
-            "forward" -> position.horizontalPosition += it.second
-            "up" -> position.depth -= it.second
-            "down" -> position.depth += it.second
-            else -> error("forbidden instruction!")
-        }
+class Day2 {
+    fun main() {
+        val file = File("src/main/kotlin/day2/input")
+        val commands = Util.log(
+            "parse commands",
+            System.currentTimeMillis(),
+            parseCommands(file)
+        ) as Commands
+        Util.log(
+            "execute simple planned course",
+            System.currentTimeMillis(),
+            executeSimplePlannedCourse(commands)
+        )
+        Util.log(
+            "execute simple planned course",
+            System.currentTimeMillis(),
+            executeAdvancedPlannedCourse(commands)
+        )
     }
 
-    return position
-}
+    data class Commands(val instructions: List<String>, val args: List<Int>)
 
-fun executeAdvancedPlannedCourse(commands: Commands): Position {
-    val position = Position(0, 0, 0)
+    fun parseCommands(file: File): Commands {
+        val commands = file.readLines()
+        val instructions = commands.map { it.substringBefore(" ") }
+        val args = commands.map { it.substringAfter(" ").toInt() }
+        return Commands(instructions, args)
+    }
 
-    (commands.instructions zip commands.args).forEach {
-        when (it.first) {
-            "forward" -> {
-                position.horizontalPosition += it.second
-                position.depth += position.aim * it.second
+    fun executeSimplePlannedCourse(commands: Commands): Int {
+        val position = Position(0, 0, 0)
+
+        (commands.instructions zip commands.args).forEach {
+            when (it.first) {
+                "forward" -> position.horizontalPosition += it.second
+                "up" -> position.depth -= it.second
+                "down" -> position.depth += it.second
+                else -> error("forbidden instruction!")
             }
-            "up" -> position.aim -= it.second
-            "down" -> position.aim += it.second
-            else -> error("forbidden instruction!")
         }
+
+        return position.multiply()
     }
 
-    return position
+    fun executeAdvancedPlannedCourse(commands: Commands): Int {
+        val position = Position(0, 0, 0)
+
+        (commands.instructions zip commands.args).forEach {
+            when (it.first) {
+                "forward" -> {
+                    position.horizontalPosition += it.second
+                    position.depth += position.aim * it.second
+                }
+                "up" -> position.aim -= it.second
+                "down" -> position.aim += it.second
+                else -> error("forbidden instruction!")
+            }
+        }
+
+        return position.multiply()
+    }
 }

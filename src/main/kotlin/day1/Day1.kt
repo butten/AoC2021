8a498caa18511171ca17eed0caa1190cb6de0435
Gustaf Johansson <Gustaf.Johansson@cpacsystems.se>
2022-01-04 15:29:40 +0100

    add tests for day1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -3,35 +3,40 @@ package day1
 import util.Util
 import java.io.File
 
-fun main() {
-    val file = File("src/main/kotlin/day1/input")
-    val measurements: List<Int> = Util.log(
-        "parse measurements",
-        System.currentTimeMillis(),
-        file.readLines().map { line -> line.toInt() }).filterIsInstance<Int>()
-    Util.log(
-        "simple sweep",
-        System.currentTimeMillis(),
-        simpleSweep(measurements)
-    )
-    Util.log(
-        "advanced sweep",
-        System.currentTimeMillis(),
-        advancedSweep(measurements)
-    )
-}
+class Day1 {
+    fun main() {
+        val file = File("src/main/kotlin/day1/input")
+        val measurements: List<Int> = Util.log(
+            "parse measurements",
+            System.currentTimeMillis(),
+            parseMeasurements(file)
+        ).filterIsInstance<Int>()
+        Util.log(
+            "simple sweep",
+            System.currentTimeMillis(),
+            simpleSweep(measurements)
+        )
+        Util.log(
+            "advanced sweep",
+            System.currentTimeMillis(),
+            advancedSweep(measurements)
+        )
+    }
 
-fun simpleSweep(measurements: List<Int>): Int {
-    return measurements.zipWithNext()
-        .filter { it.second > it.first }
-        .count()
-}
+    fun parseMeasurements(file: File): List<Int> = file.readLines().map { line -> line.toInt() }
 
-fun advancedSweep(measurements: List<Int>): Int {
-    return measurements.asSequence()        // asSequence to improve performance
-        .windowed(3)                    // create windows of 3 measurements
-        .map { it[0] + it[1] + it[2] }      // sum each window
-        .zipWithNext()                      // pair with next sum
-        .filter { it.second > it.first }
-        .count()
+    fun simpleSweep(measurements: List<Int>): Int {
+        return measurements.zipWithNext()
+            .filter { it.second > it.first }
+            .count()
+    }
+
+    fun advancedSweep(measurements: List<Int>): Int {
+        return measurements.asSequence()        // asSequence to improve performance
+            .windowed(3)                    // create windows of 3 measurements
+            .map { it[0] + it[1] + it[2] }      // sum each window
+            .zipWithNext()                      // pair with next sum
+            .filter { it.second > it.first }
+            .count()
+    }
 }
